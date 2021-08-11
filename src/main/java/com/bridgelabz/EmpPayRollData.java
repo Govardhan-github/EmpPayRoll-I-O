da819b22d0a8ffa1523d3b627b1bf8cfa6776acd
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-11 21:39:01 +0530

    ADDED : Employe Pay Roll Service Added

---
+++ src/main/java/com/bridgelabz/EmpPayRollData.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+public class EmpPayRollData {
+    /*
+    Declaring Variables And Used Constructor
+     */
+    private int id;
+    private String name;
+    private double salary;
+    public EmpPayRollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
---
+++ src/main/java/com/bridgelabz/EmpPayRollServices.java
@@ -0,0 +1,48 @@
+package com.bridgelabz;
+/*
+Importing Java Packages
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/*
+Declaring Employee Pay Roll Services
+ */
+public class EmpPayRollServices {
+  private  List<EmpPayRollData> empPayRollList;
+    public EmpPayRollServices(List<EmpPayRollData> empPayRollList) {
+        this.empPayRollList = empPayRollList;
+    }
+    /*
+    Declaring Main Method Here
+    And Calling The Methods
+     */
+    public static void main(String[] args){
+        ArrayList<EmpPayRollData> empPayRollList = new ArrayList<>();
+        EmpPayRollServices empPayRollServices = new EmpPayRollServices(empPayRollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        empPayRollServices.readEmpPayRollData(consoleInputReader);
+        empPayRollServices.writeEmpPayRollData();
+    }
+    /*
+    Declaring the Write Employee Roll Data Method
+    Printing The Details Of Employee
+     */
+    private void writeEmpPayRollData() {
+        System.out.println("Writing Employee Data To Console :" +empPayRollList);
+    }
+    /*
+    Declaring Read Emp Pay Roll Data Method
+    Reading The Details From The Scanner
+    Adding The Details To List
+     */
+    private void readEmpPayRollData( Scanner consoleInputReader) {
+        System.out.println("Enter Employee Id :");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name :");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee Salary :");
+        double salary = consoleInputReader.nextDouble();
+        empPayRollList.add(new EmpPayRollData(id,name,salary));
+    }
+}
